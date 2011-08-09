13713c494f1be11306c4e7d154147312a0dd6299
Todd Wallentine <todd@theideacenter.org>
2011-08-09 13:41:46 -0500

    Added the user and generated controller and views.

---
+++ racetrack/grails-app/domain/racetrack/User.groovy
@@ -0,0 +1,18 @@
+package racetrack
+
+class User {
+	
+	String login
+	String password
+	String role = "user"
+
+    static constraints = {
+		login(blank:false, nullable:false, unique:true)
+		password(blank:false, password:true)
+		role(inList:["admin", "user"])
+    }
+	
+	String toString() {
+		login
+	}
+}
